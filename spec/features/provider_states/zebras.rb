d3d5a78d646046da579b2676ab1bdeef21dc584e
Tucker McKnight <tmcknight@instructure.com>
2018-03-26 13:32:44 -0600

    Adding the ability to send params to the replay_interaction method

--- spec/features/provider_states/zebras.rb
+++ spec/features/provider_states/zebras.rb
@@ -20,6 +20,7 @@ Pact.provider_state "some other zebras are here" do
   set_up do
     some_data = [{'name' => 'Mark'},{'name' => 'Gertrude'}]
     File.open("tmp/a_mock_database.json", "w") { |file| file << some_data.to_json }
+    send_to_call ({"HTTP_AUTHORIZATION" => "password"})
   end
 
   tear_down do
